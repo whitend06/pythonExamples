631c8da14a1f2a3cd402e8f42fda0647092fa366
root <root@localhost.localdomain>
2020-05-22 03:13:38 -0400

    Çarpım Tablosu 0 => X

--- subject5/problem_2x3.py
+++ subject5/problem_2x3.py
@@ -1,10 +1,12 @@
 print("Çarpım Tablosu")
 for i in range(11):
-	print(str(i),end='	')
+	if(i==0):
+		print("X",end='	')
+	else:
+		print(str(i),end='	')
 print("")	
 for j in range(1,11):
 	print(str(j),end='	')
 	for k in range(1,11):
 		print(str(j*k),end='	')
-		#print(str(i),"x",str(j),"=",str(i*j),end='')
 	print("")	
